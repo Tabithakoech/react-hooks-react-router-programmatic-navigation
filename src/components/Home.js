f9c69ba1edc1591aee2d82cc28dd795dc74774f5
TabithaKoech <tabbyk1718@gmail.com>
2022-07-27 15:24:18 +0300

    Done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 
 function Home({ isLoggedIn }) {
-  if (!isLoggedIn) return <Redirect to="/login" />;
+  if (!isSignedIn) return <Redirect to="/login" />;
 
   return (
     <div>
